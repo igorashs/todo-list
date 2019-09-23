5e521e9aa32684aac6875093b46c5bfded3ae19a
igorashs <igorashsirbu@gmail.com>
2019-09-23 21:17:07 +0300

    add todo module

---
+++ src/js_modules/factories/todo.js
@@ -0,0 +1,9 @@
+export default class Todo {
+  constructor(title, date, description, priority, completed) {
+    this.title = title;
+    this.date = date;
+    this.description = description;
+    this.priority = priority;
+    this.completed = completed;
+  }
+}
