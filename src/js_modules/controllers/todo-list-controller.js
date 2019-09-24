bb289b1bfa797165fa101c7f91be5aa71f0a9025
igorashs <igorashsirbu@gmail.com>
2019-09-24 12:20:03 +0300

    add handlers for todo

--- src/js_modules/controllers/todo-list-controller.js
+++ src/js_modules/controllers/todo-list-controller.js
@@ -12,7 +12,7 @@ export default class TodoListController {
     const _todoListModel = new TodoListModel();
     const _todoListView = new TodoListView();
 
-    // add handlers
+    // add handlers for TodoListModel
     // load
     _todoListModel.on('loadProject', (prj) => {
       _todoListView.render(prj);
@@ -20,6 +20,28 @@ export default class TodoListController {
     // remove
     _todoListModel.on('removeProject', () => {
       _todoListView.displayNoProject();
+    });
+
+    // add handlers for TodoListView
+    // show full info
+    _todoListView.on('showFullInfo', (id) => {
+      // !display full info
+      console.log('full info', id);
+    });
+    // complete
+    _todoListView.on('completeTodo', (id) => {
+      // !make complete
+      console.log('completed', id);
+    });
+    // edit
+    _todoListView.on('editTodo', (id) => {
+      // ! edit
+      console.log('edit', id);
+    });
+    // delete
+    _todoListView.on('deleteTodo', (id) => {
+      // ! delete
+      console.log('delete', id);
     });
 
     return this;
