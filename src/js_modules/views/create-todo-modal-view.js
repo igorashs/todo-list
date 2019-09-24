f2193ec34cdf0377f676309cb93438952e93dc6a
igorashs <igorashsirbu@gmail.com>
2019-09-24 19:04:08 +0300

    add events

--- src/js_modules/views/create-todo-modal-view.js
+++ src/js_modules/views/create-todo-modal-view.js
@@ -9,6 +9,30 @@ export default class EditTodoMdView extends EventEmitter {
     super();
     EditTodoMdView.instance = this;
 
+    // assign events
+    // cancel
+    document
+      .querySelector('.new-todo .cancel')
+      .addEventListener('click', () => {
+        this.emit('cancelModal');
+      });
+    // create
+    document
+      .querySelector('.new-todo .create')
+      .addEventListener('click', () => {
+        this.emit('createTodo');
+      });
+
     return this;
+  }
+
+  displayModal() {
+    const modalView = document.querySelector('.new-todo');
+    modalView.parentElement.classList.remove('display-none');
+  }
+
+  closeModal() {
+    const modalView = document.querySelector('.new-todo');
+    modalView.parentElement.classList.add('display-none');
   }
 }
