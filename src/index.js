37aa8a49dbf8e1c93572b969f065d323e1d92a2a
igorashs <igorashsirbu@gmail.com>
2019-09-25 23:11:34 +0300

    fix style

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import StorageController from './js_modules/controllers/storage-controller';
 
 const storageController = new StorageController(localStorage);
 storageController.init();
+
+// responsive
+document.querySelector('.menuBtn').addEventListener('click', () => {
+  const menuView = document.querySelector('.projects-menu');
+  menuView.classList.toggle('display-flex');
+});
